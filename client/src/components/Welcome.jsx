6d697a15563b7a4d86e51b4a079aeff52134ba4e
Nazir Elpustaty <nazel607@student.liu.se>
2022-02-14 18:16:42 +0100

    Done with the app, only README file left to do

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -5,6 +5,7 @@ import {BsInfoCircle} from 'react-icons/bs';
 
 import { Loader } from "./";
 import { TransactionContext } from "../context/TransactionContext";
+import {shortenAddress} from "../utils/shortenAddress";
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
@@ -20,7 +21,7 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
-    const {connectWallet, currentAccount, formData, handleChange, sendTransaction} = useContext(TransactionContext);
+    const {connectWallet, currentAccount, formData, handleChange, sendTransaction, isLoading} = useContext(TransactionContext);
   
     const handleSubmit = (e) => {
         const {addressTo, amount, keyword, message} = formData;
@@ -76,7 +77,7 @@ const Welcome = () => {
                             </div>
                             <div>
                                 <p className="text-white font-light text-sm">
-                                    Adress
+                                    {shortenAddress(currentAccount)}
                                 </p>
                                 <p className="text-white font-semibold text-lg mt-1">
                                     Ethereum
@@ -92,7 +93,7 @@ const Welcome = () => {
 
                         <div className="h-[1px] w-full bg-gray-400 my-2"/>
 
-                        {false ? (
+                        {isLoading ? (
                             <Loader/>
                         ) : (<button 
                             type="button"
