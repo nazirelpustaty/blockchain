d414431b0243cb1708e9b678c8016774db0c2ca7
Nazir Elpustaty <98285969+nazirelpustaty@users.noreply.github.com>
2022-02-17 02:16:06 +0100

    Updated hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
   networks: {
     ropsten: {
       url: 'https://eth-ropsten.alchemyapi.io/v2/8f1xNScwyKCXArnN4mT7QSozt8wjP7m_',
-      accounts: ['70b4b9378b9d550ed453ec4127f61cebc03a4955e1d2a509c7d04ead608f6866'],
+      accounts: [''],
     },
   },
 };
